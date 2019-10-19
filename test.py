1e03bf51724de7a333974768a394aa0064838f05
spiderbeg <3368483402@qq.com>
2019-10-19 15:57:42 +0800

    添加操作不走图片

--- test.py
+++ test.py
@@ -1,4 +1,4 @@
 import timing
 
-url = 'https://detail.m.tmall.com/item.htm?id=583620173948&ali_refid=a3_430708_1006:1151882900:N::b5ba8fb29ce7b7ea84d8878b5a1bd572&ali_trackid=1_b5ba8fb29ce7b7ea84d8878b5a1bd572&spm=a2e15.11135971.4554984.1'
+url = 'https://m.suning.com/product/0070074466/646336020.html'
 timing.go(url)
